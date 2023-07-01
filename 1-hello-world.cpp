bb6af15c1a8e8477976697f41a7075f28aa9a661
pankajgouraw <pgouraw@gmail.com>
2023-07-02 02:57:11 +0530

    hello world program

---
+++ 1-hello-world.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "hello world" << endl;
+    int a = 5;
+    cout << a << "\n";
+    int b = sizeof(a);
+    cout << "size of a : " << b << "\n";
+
+    // type casting
+    int c = 'a';
+    cout << c << "\n";
+
+    char d = 98;
+    cout << d << "\n";
+
+   unsigned int g = -123;
+   cout << "unsigned " << g << "\n" ;
+}
