b000d86dd6aa331f218653433aeb5a17d1113d7b
pankajgouraw <pgouraw@gmail.com>
2023-07-09 05:43:40 +0530

    for loop

--- 6-bitwise-operator.cpp
+++ 6-bitwise-operator.cpp
@@ -2,18 +2,64 @@
 using namespace std;
 
 int main(){
-    int a =5;
-    int b=6;
+    // int a =5;
+    // int b=6;
 
-    cout<<"a&b " << (a&b) << endl;
-    cout<<"a|b " << (a|b) << endl;
-    cout<<"~b " << (~b) << endl;
-    cout<<"a^b " << (a^b) << endl;
+    // cout<<"a&b " << (a&b) << endl;
+    // cout<<"a|b " << (a|b) << endl;
+    // cout<<"~b " << (~b) << endl;
+    // cout<<"a^b " << (a^b) << endl;
 
-    int i =8;
+    // int i =8;
 
-    cout << (i++) << endl;
-    cout << (++i) << endl;
-    cout << (i--) << endl;
-    cout << (i++) << endl;
+    // cout << (i++) << endl;
+    // cout << (++i) << endl;
+    // cout << (i--) << endl;
+    // cout << (i++) << endl;
+
+    // fibonacci series
+    // 0 1 1 2 3 5 8 13 21
+    // int n;
+    // cin>>n;
+    // int a = 0;
+    // int b = 1;
+    // cout << a << " ";
+    // cout << b << " ";
+
+    // for(int i=1;i<=n; i++){
+    //     int sum = a + b;
+    //     cout << sum << " ";
+    //     a=b;
+    //     b=sum;
+
+    // }
+
+    // check number is prime or not
+
+    // int n ;
+    // cout << "enter the value of N :";
+    // cin>>n;
+    // bool isPrime = 1;
+    // for (int i =2; i < n; i++){
+
+    //     if(n%i == 0){
+    //         isPrime = 0;
+    //         break;
+    //     }
+
+    // }
+
+    //    if(isPrime == 0){
+    //          cout << "Number is not a prime number" << endl;
+    //     }else{
+    //         cout << "Number is  a prime number" << endl;
+    //     }
+
+    // continue
+    for(int i = 0; i < 5; i ++){
+        cout << "Hello dear" << endl;
+        continue;
+        cout << "hi";
+    }
+
 }
